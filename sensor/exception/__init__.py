0ce0f1259eebe026caa4a5c4214af0ca3f05dc94
rituuu <gujelaritu@gmail.com>
2025-10-30 23:56:23 +0530

    for deployment

--- sensor/exception/__init__.py
+++ sensor/exception/__init__.py
@@ -14,7 +14,7 @@ def error_message_detail(error, error_detail: sys):
 
 
 class SensorException(Exception):
-    def __init__(self, error_message, error_detail:sys):
+    def __init__(self, error_message, error_detail):
         """
         :param error_message: error message in string format
         """
